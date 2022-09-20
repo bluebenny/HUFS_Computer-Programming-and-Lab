959891fc202d0f832c4d5ab5c47d53dc435c3ed7
bluebenny <benny-kim@naver.com>
2022-09-20 17:31:20 +0900

    Complete Assignment1_3

--- Assignment/src/Assignment1_3.java
+++ Assignment/src/Assignment1_3.java
@@ -1,18 +1,8 @@
 import java.io.*;
 
 public class Assignment1_3 {
-
-	// 필요한 부분을 수정하여 작성하시오.
-	
-	/* 윤년 검사
-	if (year % 4 == 0)
-		if (year % 100 == 0) {
-			if (year % 400 == 0) System.out.println("윤년");
-		} else System.out.println("윤년");
-	*/
-	
 	public static int calcDates(int startDate, int finalDate) {
-		// Cal start YMD
+		// Cal startYMD
 		int startYear = startDate / 10000;
 		startDate = startDate % 10000;
 		
@@ -20,9 +10,9 @@ public class Assignment1_3 {
 		startDate = startDate % 100;
 		
 		int startDay = startDate;
-		// Cal start YMD End
+		// Cal startYMD 끝
 		
-		// Cal final YMD
+		// Cal finalYMD
 		int finalYear = finalDate / 10000;
 		finalDate = finalDate % 10000;
 		
@@ -30,29 +20,34 @@ public class Assignment1_3 {
 		finalDate = finalDate % 100;
 		
 		int finalDay = finalDate;
-		// Cal final YMD End
+		// Cal finalYMD 끝
 
 		int day = 0;
 
+        /* Idea: 연, 월, 일 순으로 날짜를 계산함 
+         * 
+         * ex) 20190503 ~ 20220628 계산
+         *  1부: 20200101 ~ 20220628 계산
+         *  2부: 20190601 ~ 20191213 계산
+         *  3부: 20190503 ~ 20190531 계산
+         */
+        
+        /* 1부 */
+
 		while(finalYear > startYear) {
-			if (finalMonth == 0) {
-                // finalYear == startYear 까지 연단위로 날짜를 더함.
-                // 두번째 반복부터 실행되는 부분임.
-				
+			if (finalMonth == 0) { // 연단위로 날짜를 더함. 두번째 반복부터 실행되는 부분임.
 				day += 365;
 				
-				// 윤년 검사
+				// 윤년 검사: 윤년인지 검사하고 목적에 맞는 연산을 실행함.
 				if (finalYear % 4 == 0)
 					if (finalYear % 100 == 0) {
 						if (finalYear % 400 == 0) day += 1;
 					} else day += 1;
 				// 윤년 검사 끝
 				
-			} else {
-				// finalMonth가 0이 아닐때: 반복문 처음에 실행되는 부분임.
+			} else { // finalMonth가 0이 아닐때: 처음 반복 시에 실행되는 부분임.
 				while (finalMonth > 0) {
-					// finalMonth에 따른 월의 일수를 monthDay 변수에 저장함
-					// Cal monthDay
+					// Cal monthDay: month 값에 따른 월의 일수를 monthDay 변수에 저장함
 					int monthDay = 0;
 					switch(finalMonth) {
 						case 1: 
@@ -80,7 +75,7 @@ public class Assignment1_3 {
 							// 윤년 검사 끝
 							break;
 					}
-					// Cal monthDay End
+					// Cal monthDay 끝
 					if (finalDay < monthDay) {
                         day += finalDay;
                         finalDay = 50;
@@ -90,6 +85,8 @@ public class Assignment1_3 {
 			}
             finalYear -= 1;
 		}
+
+        /* 2부 */
 
         if (finalMonth == 0) finalMonth = 12;
         while (finalMonth > startMonth) {
@@ -123,10 +120,12 @@ public class Assignment1_3 {
                     // 윤년 검사 끝
                     break;
             }
-            // Cal monthDay End
+            // Cal monthDay 끝
             day += monthDay;
             finalMonth -= 1;
         }
+
+        /* 3부 */
 
 		// Cal monthDay
 		int monthDay = 0;
@@ -156,7 +155,7 @@ public class Assignment1_3 {
 				// 윤년 검사 끝
 				break;
 		}
-		// Cal monthDay End
+		// Cal monthDay 끝
 		
         if (finalDay == 50) finalDay = monthDay;
 		day += finalDay - startDay + 1;
