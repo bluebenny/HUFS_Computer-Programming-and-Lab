9994e1d819c01b22e0a3c2a7b7258aaecda0614c
bluebenny <benny-kim@naver.com>
2022-10-06 18:44:59 +0900

    2022.10.06. Class

--- Example/src/Car.java
+++ Example/src/Car.java
@@ -1,14 +1,23 @@
-class Car {
+public class Car {
+    String company = "현대자동차";
+    String model;
     String color;
-    int speed;
+    int maxSpeed;
 
-    Car(String color, int cc) {
-        this.color = color;
-        // color = color;
-        speed = cc;
+    Car() {
     }
 
-    // void method() {
-        // speed = 10;
-    // }
+    Car(String model) {
+        this(model, "은색", 250);
+    }
+
+    Car(String model, String color) {
+        this(model, color, 250);
+    }
+
+    Car(String model, String color, int maxSpeed) {
+        this.model = model;
+        this.color = color;
+        this.maxSpeed = maxSpeed;
+    }
 }
