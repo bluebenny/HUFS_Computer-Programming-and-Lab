ca79427903e16b9eb03079503ac2b24b94fee60f
bluebenny <benny-kim@naver.com>
2022-10-06 15:56:28 +0900

    2022.09.19. ~ 2022.09.29. Class

---
+++ Example/src/Car.java
@@ -0,0 +1,14 @@
+class Car {
+    String color;
+    int speed;
+
+    Car(String color, int cc) {
+        this.color = color;
+        // color = color;
+        speed = cc;
+    }
+
+    // void method() {
+        // speed = 10;
+    // }
+}
---
+++ Example/src/Person.java
@@ -0,0 +1,6 @@
+class Person {
+    void method() {
+        // Car myCar = new Car();
+        // myCar.speed = 60;
+    }
+}
