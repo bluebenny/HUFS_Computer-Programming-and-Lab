8fc8b99c8fcbb97b89cde0b1d74c10e2ec75e1eb
bluebenny <benny-kim@naver.com>
2022-11-10 18:33:20 +0900

    2022.11.10. Class

---
+++ Example/src/ch08/RemoteControlExample.java
@@ -0,0 +1,29 @@
+package ch08;
+
+public class RemoteControlExample {
+    public static void main(String[] args) {
+        RemoteControl rm = createControl();
+        rm.turnOn();
+
+        
+    }
+
+    public static RemoteControl createControl() {
+        class MyClass implements RemoteControl {
+            public void turnOn() {
+                System.out.println("ON");
+            }
+        }
+
+        RemoteControl rm = new MyClass();
+        return rm;
+    }
+
+    public static RemoteControl createContorl2() {
+        return new RemoteControl() { // 익명클래스의 좋은 예.
+            public void turnOn() {
+                System.out.println("ON");
+            }
+        };
+    }
+}
