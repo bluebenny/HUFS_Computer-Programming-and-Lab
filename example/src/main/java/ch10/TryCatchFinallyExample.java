d0358864ed69b5485237b20c4fda237a0908a9ad
bluebenny <benny-kim@naver.com>
2022-11-21 19:22:58 +0900

    2022.11.21. Class

---
+++ example/src/main/java/ch10/TryCatchFinallyExample.java
@@ -0,0 +1,12 @@
+package ch10;
+
+public class TryCatchFinallyExample {
+    public static void main(String[] args) {
+        // try {
+        //     Class clazz = Class.forName("java.lang.String2"); // 클래스 파일을 메모리에 적재하라.
+        // } catch (ClassNotFoundException e) {
+        //     // TODO Auto-generated catch block
+        //     e.printStackTrace();
+        // }
+    }
+}
